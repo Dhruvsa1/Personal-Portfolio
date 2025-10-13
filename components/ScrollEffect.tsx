471f22539a9ad0b2858d5fb61f88a9eca2ddd4d5
Dhruvsai Dhulipudi <ddhruvsai@outlook.com>
2025-10-13 11:59:34 -0400

    Add name unraveling effect: DD slides out to reveal full name

--- components/ScrollEffect.tsx
+++ components/ScrollEffect.tsx
@@ -3,7 +3,18 @@ import './ScrollEffect.css';
 export default function ScrollEffect() {
   return (
     <div className="stuck-grid">
-      <div className="grid-item">one</div>
+      <div className="grid-item name-reveal">
+        <div className="name-container">
+          <div className="name-part">
+            <span className="big-d">D</span>
+            <span className="slide-out">hruvsai</span>
+          </div>
+          <div className="name-part">
+            <span className="big-d">D</span>
+            <span className="slide-out">hulipudi</span>
+          </div>
+        </div>
+      </div>
       <div className="grid-item">two</div>
       <div className="grid-item">three</div>
       <div className="grid-item">four</div>
